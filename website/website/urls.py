39b19780ecf4c6e2bd6c4b66d22ac5430d98d1b6
Michal Paszkowski <michal.paszkowski@outlook.com>
2022-09-21 03:30:39 +0200

    [website] Add URLs

--- website/website/urls.py
+++ website/website/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('api/', include('dispatcher.urls')),
+    path('', include('viewer.urls')),
 ]
