2787c5c0761fcf2e4ef485c678854eb51016260d
Michal Paszkowski <michal.paszkowski@outlook.com>
2022-09-22 18:32:15 +0200

    [website] Read development SECRET_KEY from env

--- website/website/settings.py
+++ website/website/settings.py
@@ -10,17 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4gsczw(ab8zms9femu%f3rtykswlak$g@$xgw-6fv35m*^0hn7'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
